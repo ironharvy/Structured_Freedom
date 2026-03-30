c69650ed2f13b3e0e3d8ba89be288fd2da8f451a
IronHarvy <ironharvy@gmail.com>
2026-03-29 17:32:20 -0700

    lint error fixed

--- src/structured_freedom/models/__init__.py
+++ src/structured_freedom/models/__init__.py
@@ -1,13 +1,13 @@
 from structured_freedom.models.item import Item
 from structured_freedom.models.location import Location
-from structured_freedom.models.world_state import WorldState
-from structured_freedom.models.player import Player, PlayerStats, QuestState
 from structured_freedom.models.npc import (
     NPC,
     NPCDialogueContext,
     NPCMemory,
     NPCMemoryEntry,
 )
+from structured_freedom.models.player import Player, PlayerStats, QuestState
+from structured_freedom.models.world_state import WorldState
 
 __all__ = ["Item", 
     "Location",
